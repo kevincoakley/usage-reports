7e87d9fff875b2abbe8d9df4424e88ae19841439
Kevin Coakley <kcoakley@sdsc.edu>
2015-09-30 15:56:25 -0700

    Create graph_data_list from usage_list

--- databricksusagereport/graph.py
+++ databricksusagereport/graph.py
@@ -17,15 +17,19 @@ class Graph:
     def create(usage_list):
 
         graph_data = ""
+        names = []
 
         for usage in usage_list:
+            if usage["name"] not in names:
+                names.append(usage["name"])
+
             graph_data += Graph.graph_data_template % (usage["name"],
                                                        "'%s'" % usage["date"],
                                                        usage["NumWorkers"],
                                                        usage["name"],
                                                        "lines+markers")
 
-        graph_data_list = "team1, team2, team3"
+        graph_data_list = ', '.join(map(str, names))
         title = "Cluster Worker Usage"
 
         filled_template = Graph.databricks_graph_data_template % (graph_data,
