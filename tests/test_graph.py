c6b1ebe5b2b6706e30cac4847ff3d6e3de2a90e4
Kevin Coakley <kcoakley@sdsc.edu>
2015-10-01 10:12:34 -0700

    Refactored tests and variable names

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -6,90 +6,89 @@ from databricksusagereport.graph.databricks import DataBricksGraph
 class GraphTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_usage_no_history_file = "tests/graph/databricks_usage_no_history.js"
-        self.db_usage_no_history = [{"name": "team1",
-                                     "NumWorkers": 4,
-                                     "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
-                                    {"name": "team2",
-                                     "NumWorkers": 8,
-                                     "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
-                                    {"name": "team3",
-                                     "NumWorkers": 6,
-                                     "date": datetime.datetime(2015, 10, 1, 1, 0, 0)}]
-        self.db_usage_with_history_file = "tests/graph/databricks_usage_with_history.js"
-        self.db_usage_with_history_ordered = [{"name": "team1",
-                                               "NumWorkers": 4,
-                                               "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
-                                              {"name": "team2",
-                                               "NumWorkers": 8,
-                                               "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
-                                              {"name": "team3",
-                                               "NumWorkers": 6,
-                                               "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
-                                              {"name": "team1",
-                                               "NumWorkers": 4,
-                                               "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
-                                              {"name": "team2",
-                                               "NumWorkers": 6,
-                                               "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
-                                              {"name": "team3",
-                                               "NumWorkers": 8,
-                                               "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
-                                              {"name": "team1",
-                                               "NumWorkers": 4,
-                                               "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
-                                              {"name": "team2",
-                                               "NumWorkers": 10,
-                                               "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
-                                              {"name": "team3",
-                                               "NumWorkers": 6,
-                                               "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
-                                              {"name": "team1",
-                                               "NumWorkers": 6,
-                                               "date": datetime.datetime(2015, 10, 1, 4, 0, 0)},
-                                              {"name": "team2",
-                                               "NumWorkers": 8,
-                                               "date": datetime.datetime(2015, 10, 1, 4, 0, 0)},
-                                              {"name": "team3",
-                                               "NumWorkers": 8,
-                                               "date": datetime.datetime(2015, 10, 1, 4, 0, 0)}]
-        self.db_usage_with_history_unordered = [{"name": "team1",
-                                                 "NumWorkers": 4,
-                                                 "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
-                                                {"name": "team2",
-                                                 "NumWorkers": 10,
-                                                 "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
-                                                {"name": "team3",
-                                                 "NumWorkers": 6,
-                                                 "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
-                                                {"name": "team1",
-                                                 "NumWorkers": 4,
-                                                 "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
-                                                {"name": "team2",
-                                                 "NumWorkers": 8,
-                                                 "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
-                                                {"name": "team3",
-                                                 "NumWorkers": 6,
-                                                 "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
-                                                {"name": "team1",
-                                                 "NumWorkers": 4,
-                                                 "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
-                                                {"name": "team2",
-                                                 "NumWorkers": 6,
-                                                 "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
-                                                {"name": "team3",
-                                                 "NumWorkers": 8,
-                                                 "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
-
-                                                {"name": "team1",
-                                                 "NumWorkers": 6,
-                                                 "date": datetime.datetime(2015, 10, 1, 4, 0, 0)},
-                                                {"name": "team2",
-                                                 "NumWorkers": 8,
-                                                 "date": datetime.datetime(2015, 10, 1, 4, 0, 0)},
-                                                {"name": "team3",
-                                                 "NumWorkers": 8,
-                                                 "date": datetime.datetime(2015, 10, 1, 4, 0, 0)}]
+        self.db_usage_single_file = "tests/graph/databricks_usage_single.js"
+        self.db_usage_single = [{"name": "team1",
+                                 "NumWorkers": 4,
+                                 "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
+                                {"name": "team2",
+                                 "NumWorkers": 8,
+                                 "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
+                                {"name": "team3",
+                                 "NumWorkers": 6,
+                                 "date": datetime.datetime(2015, 10, 1, 1, 0, 0)}]
+        self.db_usage_multiple_file = "tests/graph/databricks_usage_multiple.js"
+        self.db_usage_multiple = [{"name": "team1",
+                                   "NumWorkers": 4,
+                                   "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
+                                  {"name": "team2",
+                                   "NumWorkers": 8,
+                                   "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
+                                  {"name": "team3",
+                                   "NumWorkers": 6,
+                                   "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
+                                  {"name": "team1",
+                                   "NumWorkers": 4,
+                                   "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
+                                  {"name": "team2",
+                                   "NumWorkers": 6,
+                                   "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
+                                  {"name": "team3",
+                                   "NumWorkers": 8,
+                                   "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
+                                  {"name": "team1",
+                                   "NumWorkers": 4,
+                                   "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
+                                  {"name": "team2",
+                                   "NumWorkers": 10,
+                                   "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
+                                  {"name": "team3",
+                                   "NumWorkers": 6,
+                                   "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
+                                  {"name": "team1",
+                                   "NumWorkers": 6,
+                                   "date": datetime.datetime(2015, 10, 1, 4, 0, 0)},
+                                  {"name": "team2",
+                                   "NumWorkers": 8,
+                                   "date": datetime.datetime(2015, 10, 1, 4, 0, 0)},
+                                  {"name": "team3",
+                                   "NumWorkers": 8,
+                                   "date": datetime.datetime(2015, 10, 1, 4, 0, 0)}]
+        self.db_usage_multiple_unordered = [{"name": "team1",
+                                             "NumWorkers": 4,
+                                             "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
+                                            {"name": "team2",
+                                             "NumWorkers": 10,
+                                             "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
+                                            {"name": "team3",
+                                             "NumWorkers": 6,
+                                             "date": datetime.datetime(2015, 10, 1, 3, 0, 0)},
+                                            {"name": "team1",
+                                             "NumWorkers": 4,
+                                             "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
+                                            {"name": "team2",
+                                             "NumWorkers": 8,
+                                             "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
+                                            {"name": "team3",
+                                             "NumWorkers": 6,
+                                             "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
+                                            {"name": "team2",
+                                             "NumWorkers": 6,
+                                             "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
+                                            {"name": "team3",
+                                             "NumWorkers": 8,
+                                             "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
+                                            {"name": "team2",
+                                             "NumWorkers": 8,
+                                             "date": datetime.datetime(2015, 10, 1, 4, 0, 0)},
+                                            {"name": "team1",
+                                             "NumWorkers": 4,
+                                             "date": datetime.datetime(2015, 10, 1, 2, 0, 0)},
+                                            {"name": "team1",
+                                             "NumWorkers": 6,
+                                             "date": datetime.datetime(2015, 10, 1, 4, 0, 0)},
+                                            {"name": "team3",
+                                             "NumWorkers": 8,
+                                             "date": datetime.datetime(2015, 10, 1, 4, 0, 0)}]
         self.aws_usage = [{"name": "team1",
                            "Cost": 1.00,
                            "date": datetime.datetime(2015, 10, 1, 1, 0, 0)},
@@ -97,26 +96,26 @@ class GraphTestCase(unittest.TestCase):
                            "Cost": 1.00,
                            "date": datetime.datetime(2015, 10, 1, 1, 0, 0)}]
 
-    def test_databricks_graph_no_history(self):
+    def test_databricks_graph_single(self):
         databricks_graph = DataBricksGraph()
 
-        with open(self.db_usage_no_history_file, "r") as f:
+        with open(self.db_usage_single_file, "r") as f:
             valid_ouput = f.read()
 
-        self.assertEqual(databricks_graph.create(self.db_usage_no_history), valid_ouput)
+        self.assertEqual(databricks_graph.create(self.db_usage_single), valid_ouput)
 
-    def test_databricks_graph_with_history_ordered(self):
+    def test_databricks_graph_multiple(self):
         databricks_graph = DataBricksGraph()
 
-        with open(self.db_usage_with_history_file, "r") as f:
+        with open(self.db_usage_multiple_file, "r") as f:
             valid_ouput = f.read()
 
-        self.assertEqual(databricks_graph.create(self.db_usage_with_history_ordered), valid_ouput)
+        self.assertEqual(databricks_graph.create(self.db_usage_multiple), valid_ouput)
 
-    def test_databricks_graph_with_history_unordered(self):
+    def test_databricks_graph_multiple_unordered(self):
         databricks_graph = DataBricksGraph()
 
-        with open(self.db_usage_with_history_file, "r") as f:
+        with open(self.db_usage_multiple_file, "r") as f:
             valid_ouput = f.read()
 
-        self.assertEqual(databricks_graph.create(self.db_usage_with_history_unordered), valid_ouput)
+        self.assertEqual(databricks_graph.create(self.db_usage_multiple_unordered), valid_ouput)
