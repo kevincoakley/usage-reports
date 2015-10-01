12d9152bb2a63d27cbad2f25ce9889488aca1039
Kevin Coakley <kcoakley@sdsc.edu>
2015-10-01 16:56:14 -0700

    Created upload and download methods for the GitHub class

--- databricksusagereport/github.py
+++ databricksusagereport/github.py
@@ -1,10 +1,54 @@
 #!/usr/bin/env python
 
+import os.path
+# noinspection PyPackageRequirements
+import github3
+
 
 class GitHub:
+
+    user = "kevincoakley"
+    repository = "databricks-usage-report"
 
     def __init__(self, token):
         self.token = token
 
+    def download(self, path):
+        filename = os.path.basename(path)
+        directory = os.path.dirname(path)
+
+        github = github3.login(token=self.token)
+
+        repo = github.repository(GitHub.user, GitHub.repository)
+
+        directory_contents = repo.directory_contents(directory)
+
+        for content in directory_contents:
+
+            if content[0] == filename:
+                return repo.blob(content[1].sha).decoded
+
+        return None
+
     def upload(self, path, content):
-        pass
+        commit_comment = "Automated Commit: %s" % path
+        filename = os.path.basename(path)
+        directory = os.path.dirname(path)
+
+        github = github3.login(token=self.token)
+
+        repo = github.repository(GitHub.user, GitHub.repository)
+
+        directory_contents = repo.directory_contents(directory)
+
+        github_file = None
+
+        for content in directory_contents:
+
+            if content[0] == filename:
+                github_file = content[1]
+
+        if github_file is None:
+            repo.create_file(path, commit_comment, content)
+        else:
+            github_file.update(commit_comment, content)
