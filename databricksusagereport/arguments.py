c49af2edb7fa7e3a94cec756d697d99a2600073f
Kevin Coakley <kcoakley@sdsc.edu>
2015-10-09 17:20:51 -0700

    Added command line argument for the type of storage

---
+++ databricksusagereport/arguments.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    storage_type = parser.add_mutually_exclusive_group(required=True)
+    storage_type.add_argument('--github',
+                              dest="github_storage",
+                              action='store_true')
+    storage_type.add_argument('--aws',
+                              dest="aws_storage",
+                              action='store_true')
+
+    return vars(parser.parse_args())
