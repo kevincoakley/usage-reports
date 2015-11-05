05001d7771d1678bb48d8b83a87078dd003c5e23
Kevin Coakley <kcoakley@sdsc.edu>
2015-11-04 16:03:58 -0800

    Made the bucket and the path variable

--- databricksusagereport/databricksawscost.py
+++ databricksusagereport/databricksawscost.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python
 
 import os
+import re
 import logging
+import argparse
 from datetime import datetime
 from pkg_resources import resource_string
-import databricksusagereport.arguments
 from databricksusagereport.usage.aws import AwsUsage
 from databricksusagereport.storage.storage import Storage
 from databricksusagereport.graph.aws import AWSGraph
 
 
-def main():
-    args = databricksusagereport.arguments.parse_arguments()
+def main(bucket=None, path=None):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-b",
+                        metavar="bucket",
+                        dest="bucket",
+                        help="AWS bucket where the billing reports are saved.",
+                        required=True)
+
+    parser.add_argument("-p",
+                        metavar="path",
+                        dest="path",
+                        help="Path to the billing report. The billing report for the current month "
+                             "will be used if no path is specified.",
+                        default=None,
+                        required=False)
+
+    parser.add_argument('--debug',
+                        dest="debug",
+                        action='store_true')
+
+    args = vars(parser.parse_args())
 
     if args["debug"] is True:
         log_level = logging.DEBUG
@@ -24,6 +45,15 @@ def main():
 
     logging.info('STARTED: databricks-aws-cost')
 
+    logging.debug("bucket: %s, path: %s, args[\"bucket\"]: %s, args[\"path\"]: %s", bucket, path,
+                  args["bucket"], args["path"])
+
+    if bucket is None:
+        bucket = args["bucket"]
+
+    if path is None:
+        path = args["path"]
+
     aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID", None)
     aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY", None)
 
@@ -34,13 +64,9 @@ def main():
         logging.debug("aws_access_key_id: %s", aws_access_key_id)
         logging.debug("aws_secret_access_key: %s", aws_secret_access_key[:3])
 
-    if args["aws_storage"] and aws_access_key_id is not None and aws_secret_access_key is not None:
-        logging.info("Using AWS storage")
-        so = Storage(aws_access_key_id=aws_access_key_id,
-                     aws_secret_access_key=aws_secret_access_key)
-    else:
-        logging.info("No storage method found, check environment variables")
-        return False
+    logging.info("Using AWS storage")
+    so = Storage(aws_access_key_id=aws_access_key_id,
+                 aws_secret_access_key=aws_secret_access_key)
 
     storage = so.get_storage()
 
@@ -49,7 +75,13 @@ def main():
 
     aws_usage = AwsUsage(aws_access_key_id, aws_secret_access_key)
     logging.info("Downloading and parsing the latest AWS detailed billing report")
-    aws_clusters_cost = aws_usage.get()
+
+    logging.info("Bucket: %s, Path: %s", bucket, path)
+    if path is None:
+        aws_clusters_cost = aws_usage.get_current(bucket)
+    else:
+        aws_clusters_cost = aws_usage.get(bucket, path)
+
     logging.debug("aws_cost: %s", aws_clusters_cost)
 
     aws_graph = AWSGraph()
@@ -57,12 +89,19 @@ def main():
 
     for key, value in aws_cost_graphs.iteritems():
         # Construct the upload_directory based on the cluster name and month
-        upload_directory = "clusters/cost/%s/%s/%s" % (key,
-                                                       datetime.now().strftime("%Y"),
-                                                       datetime.now().strftime("%m"))
-        if args["aws_storage"]:
-            logging.info("Uploading indexes to AWS: %s", upload_directory)
-            storage.upload_index(upload_directory)
+        if path is None:
+            year = datetime.now().strftime("%Y")
+            month = datetime.now().strftime("%m")
+            logging.debug("year: %s, month: %s (system time)", year, month)
+        else:
+            year = re.search('-(\d{4})-', path).group(1)
+            month = re.search('-(\d{2})\.', path).group(1)
+            logging.debug("year: %s, month: %s (path)", year, month)
+
+        upload_directory = "clusters/cost/%s/%s/%s" % (key, year, month)
+
+        logging.info("Uploading indexes to AWS: %s", upload_directory)
+        storage.upload_index(upload_directory)
 
         logging.info("Upload directory: %s", upload_directory)
 
