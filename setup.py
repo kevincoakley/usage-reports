cfe023e9eb4294b5946e88ddc40dda8bbe1863a9
Kevin Coakley <kcoakley@sdsc.edu>
2015-11-06 15:06:59 -0800

    Updated to version 0.2.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def readme():
 
 
 setup(name="usage-reports",
-      version="0.2.0",
+      version="0.2.1",
       description="Create usage and cost reports for AWS and Databricks services",
       long_description=readme(),
       author="Kevin Coakley",
