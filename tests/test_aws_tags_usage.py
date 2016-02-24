cb5201f1e8d28afbe7eef66830c38ec8bc7d4cf7
Kevin Coakley <kcoakley@sdsc.edu>
2016-02-23 18:11:34 -0800

    Updated test for the user:course tag

--- tests/test_aws_tags_usage.py
+++ tests/test_aws_tags_usage.py
@@ -6,11 +6,13 @@ from usagereports.usage.aws_tags import AwsTagsUsage
 class AWSTagsUsageTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.aws_usage_csv_file_string = "tests/aws/aws-billing-detailed-line-" \
-                                         "items-with-resources-and-tags-blended.csv"
+        self.aws_usage_csv_cluster_file_string = "tests/aws/aws-billing-detailed-line-" \
+                                                 "items-with-resources-and-tags-blended-cluster.csv"
+        self.aws_usage_csv_course_file_string = "tests/aws/aws-billing-detailed-line-" \
+                                                "items-with-resources-and-tags-blended-course.csv"
         self.no_cluster_aws_usage_csv_file_string = "tests/aws/aws-billing-detailed-line-items-" \
                                                     "with-resources-and-tags-blended-missing-" \
-                                                    "cluster-tag.csv"
+                                                    "tags.csv"
 
         self.aws_usage_output = {"admin_c1":
                                  {"date": [datetime.datetime(2015, 9, 30, 0, 0)],
@@ -24,10 +26,18 @@ class AWSTagsUsageTestCase(unittest.TestCase):
                                   "cost": [0.88]}
                                  }
 
-    def test_aws_tags_usage_file(self):
+    def test_aws_tags_cluster_usage_file(self):
         aws_usage = AwsTagsUsage()
 
-        with open(self.aws_usage_csv_file_string, "r") as f:
+        with open(self.aws_usage_csv_cluster_file_string, "r") as f:
+            valid_output = f.read()
+
+        self.assertEqual(aws_usage.parse(valid_output), self.aws_usage_output)
+
+    def test_aws_tags_course_usage_file(self):
+        aws_usage = AwsTagsUsage()
+
+        with open(self.aws_usage_csv_course_file_string, "r") as f:
             valid_output = f.read()
 
         self.assertEqual(aws_usage.parse(valid_output), self.aws_usage_output)
